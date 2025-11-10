90dff9002ebc45c5b7a4964fc1971871a85f8ec1
rigxter <188875106+rigxter@users.noreply.github.com>
2025-11-10 20:11:03 +0100

    Refactor Homepage component to include ServiceBanner and improve layout structure

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,14 +1,21 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Herobanner from "../../components/Herobanner/Herobanner";
+import ServiceBanner from "../../components/ServiceBanner/ServiceBanner";
+
 import styles from "./Homepage.module.css";
 
 export default function Homepage() {
     return (
-        <div className={styles.backgroundImageLayer}>
-            <Navbar></Navbar>
-            <div className="container">
-                <Herobanner></Herobanner>
+        <>
+            <div className={styles.backgroundImageLayer}>
+                <Navbar />
+                <div className="container">
+                    <Herobanner />
+                </div>
             </div>
-        </div>
+            <div className="container">
+                <ServiceBanner />
+            </div>
+        </>
     );
 }
