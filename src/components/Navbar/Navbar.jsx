59b9b2d5f552cabe33cf9c93ebc3a84d35f769b1
rigxter <188875106+rigxter@users.noreply.github.com>
2025-11-10 17:54:02 +0100

    Add Navbar component with scroll effect and icons

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import styles from "./Navbar.module.css";
+import Icon from "@mdi/react";
+import { mdiMagnify, mdiCart, mdiHeart, mdiAccount } from "@mdi/js";
+import { useEffect, useState } from "react";
+
+export default function Navbar() {
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.scrollY);
+        window.addEventListener("scroll", onScroll, { passive: true });
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
+    return (
+        <nav className={offset > 20 && styles.scroll}>
+            <div className={styles.navbarContentContainer}>
+                <div className={styles.mainLogo}>Logo</div>
+                <ul className={styles.navText}>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Shop</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+                <ul className={styles.navIcon}>
+                    <li>
+                        <a href="">
+                            <Icon path={mdiAccount} size={1} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <Icon path={mdiHeart} size={1} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <Icon path={mdiCart} size={1} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <Icon path={mdiMagnify} size={"1.7rem"} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
