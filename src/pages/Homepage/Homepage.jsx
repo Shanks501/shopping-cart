7a9210502070775a56c5430dd151394530c93c98
rigxter <188875106+rigxter@users.noreply.github.com>
2025-11-17 17:18:41 +0100

    Fix Navbar component disappearing when scrolling past Herobanner

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,20 +1,22 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Herobanner from "../../components/Herobanner/Herobanner";
 import ServiceBanner from "../../components/ServiceBanner/ServiceBanner";
+import ProductGrid from "../../components/ProductGrid/ProductGrid";
 
 import styles from "./Homepage.module.css";
 
 export default function Homepage() {
     return (
         <>
+            <Navbar />
             <div className={styles.backgroundImageLayer}>
-                <Navbar />
                 <div className="container">
                     <Herobanner />
                 </div>
             </div>
             <div className="container">
                 <ServiceBanner />
+                <ProductGrid />
             </div>
         </>
     );
