363e8310559268d82623de36701ba8b7513777ac
rigxter <188875106+rigxter@users.noreply.github.com>
2025-11-22 18:57:05 +0100

    Add custom hook useFetchData for fetching product data

---
+++ src/hooks/useFetchData.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function useFetchData(query) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${query}`, { mode: "cors" })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error");
+                }
+                return response.json();
+            })
+            .then((response) => setData(response))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return { data, loading, error };
+}
