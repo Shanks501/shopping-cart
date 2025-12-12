7ea02aee98ef0ca8ffba227639ca46e367eff71e
rigxter <188875106+rigxter@users.noreply.github.com>
2025-12-12 18:10:05 +0100

    Add SalesBanner component with styles and background image

---
+++ src/components/SalesBanner/SalesBanner.jsx
@@ -0,0 +1,31 @@
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
+
+import styles from "./SalesBanner.module.css";
+
+export default function SalesBanner() {
+    return (
+        <div className={styles.seactionSalesBanner}>
+            <div className={styles.cardText}>
+                <h4>
+                    Get <i>20% OFF</i> on
+                    <br /> your first purchase
+                </h4>
+                <p>Sign Up for our newsletter and never miss any offers</p>
+            </div>
+            <div className={styles.cardRegister}>
+                <Input
+                    type={"email"}
+                    id={"email"}
+                    placeholderText={"Enter Your Email Address"}
+                    styleClass={"input-component"}
+                />
+                <Button
+                    type={"button"}
+                    text={"REGISTER IT NOW"}
+                    styleClass={"primaryButton-hover"}
+                />
+            </div>
+        </div>
+    );
+}
