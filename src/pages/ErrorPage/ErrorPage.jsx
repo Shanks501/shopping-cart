b4ca15c37a7e6efd968c36f3f8d86cd1f3804b93
rigxter <188875106+rigxter@users.noreply.github.com>
2025-12-27 16:58:15 +0100

    Add ErrorPage component with 404 message and styling

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+import Styles from "./ErrorPage.module.css";
+import Button from "../../components/Button/Button";
+
+export default function ErrorPage() {
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className={Styles.errorPageMessage}>
+                    <h1>404</h1>
+                    <p>Page not found</p>
+                    {/* <p>We are sorry, but the page you requested was not found</p> */}
+                    <Link to={"/"}>
+                        <Button type={"button"} text={"Go Home"} styleClass={"primaryButton"} />
+                    </Link>
+                </div>
+            </div>
+
+            <Footer />
+        </>
+    );
+}
