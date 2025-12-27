77cb298c1f58304ecbc1ce2db266dfcc86d32813
rigxter <188875106+rigxter@users.noreply.github.com>
2025-12-27 16:57:28 +0100

    Set up routing with react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
 import App from "./App.jsx";
+import Shop from "./pages/Shop/Shop.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "shop",
+        element: <Shop />,
+    },
+    {
+        path: "contact",
+        element: <Contact />,
+    },
+]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>
 );
