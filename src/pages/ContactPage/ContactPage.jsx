c301fe56f74c7633a8a2e8f1e909aec8610dde7a
rigxter <188875106+rigxter@users.noreply.github.com>
2026-01-22 22:02:52 +0100

    Add ContactPage component with Navbar

---
+++ src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+export default function Contact() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <br />
+            <h1>Contact</h1>
+        </>
+    );
+}
